968009bdba18715bc33b51ae05ecfdb9b95698e2
Caio Santos <caiosantos.portugal@hotmail.com>
2021-10-14 22:03:41 -0300

    Alteracão feita na Classe Main

--- MyBankAppConsole/Cliente.cs
+++ MyBankAppConsole/Cliente.cs
@@ -11,5 +11,37 @@ namespace MyBankAppConsole
         public string nome;
         public string cpf;
         public string prof;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getCPF()
+        {
+            return this.cpf;
+        }
+
+        public void setCPF(string CPF)
+        {
+            this.cpf = CPF;
+        }
+
+        public string getProfissao()
+        {
+            return this.prof;
+        }
+
+        public void setProfissao(string profissao)
+        {
+            this.prof = profissao;
+        }
+
+
     }
 }
--- MyBankAppConsole/Program.cs
+++ MyBankAppConsole/Program.cs
@@ -6,7 +6,17 @@ namespace MyBankAppConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente1 = new Cliente();
+            cliente1.nome = "Caio";
+            cliente1.cpf = "000.000.000.00";
+            cliente1.prof = "Service Desk";
+
+          
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+            contaCorrente.titular = cliente1;
+            contaCorrente.agencia = 7375;
+            contaCorrente.conta = 00001;
         }
     }
 }
