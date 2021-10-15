640f69fa40c128ec3d12f907c2bf407ace876744
Caio Santos <caiosantos.portugal@hotmail.com>
2021-10-14 21:33:40 -0300

    Adicionada Classe ContaCorrente

---
+++ MyBankAppConsole/ContaCorrente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBankAppConsole
+{
+    class ContaCorrente
+    {
+        // O titular será um cliente.
+        public Cliente titular;
+        public int agencia;
+        public int conta;
+
+        private double saldo = 100.00;
+    }
+}
