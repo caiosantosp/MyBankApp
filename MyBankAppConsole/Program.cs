7e0ca1902d4a009bc221c5239de296b4c326b718
Caio Santos <caiosantos.portugal@hotmail.com>
2021-10-14 21:10:00 -0300

    Adicionar arquivos de projeto.

---
+++ MyBankAppConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBankAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
